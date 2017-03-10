22c42cc37ccaeb29ec81aa8893a233e6c3a31058
theunsb <theuns@zenaptix.com>
2017-03-10 02:25:35 +0200

    initial commit with code files and compiled jars

---
+++ src/co/za/tbreyten/tests/ProgrammerTaskA.java
@@ -0,0 +1,34 @@
+package co.za.tbreyten.tests;
+
+/**
+ * Created by theunsb on 17/03/10.
+ */
+public class ProgrammerTaskA {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            try {
+                int integerArg = Integer.parseInt(args[0]);
+
+                System.out.println("occurrences of 1: " + getCountOfOnes(integerArg));
+            }
+            catch (NumberFormatException e) {
+                System.out.println(args[i] + " is not an integer.");
+            }
+        }
+    }
+
+    public static int getCountOfOnes(int n) {
+
+        if (n < 1) {
+            return 0;
+        }
+
+        String binaryRepresentation = Integer.toBinaryString(n);
+
+        System.out.println("binary representation of " + n + " is " + binaryRepresentation);
+
+        return binaryRepresentation.length() - binaryRepresentation.replace("1", "").length();
+    }
+}
---
+++ src/co/za/tbreyten/tests/ProgrammerTaskB.java
@@ -0,0 +1,35 @@
+package co.za.tbreyten.tests;
+
+/**
+ * Created by theunsb on 17/03/10.
+ */
+public class ProgrammerTaskB {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            if (isPalindrome(args[i])) {
+                System.out.println(args[i] + " is a Palindrome");
+            }
+            else {
+                System.out.println(args[i] + " is NOT a Palindrome");
+            }
+        }
+    }
+
+    public static boolean isPalindrome(String term) {
+
+        for (int i = 0; i < term.length(); i++) {
+
+            System.out.println(i + " = " + term.substring(0, i + 1));
+            System.out.println((term.length() - 1) - i +  " = " + term.substring((term.length() - 1) - i));
+
+            if (term.charAt(i) != term.charAt((term.length() - 1) - i)) {
+                return false;
+            }
+
+            System.out.println();
+        }
+        return true;
+    }
+}
